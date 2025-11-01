ec5d294885dcaace287b66c1a0f21f7d21755e44
Yahia <noneedfor@mail.com>
2025-11-01 20:29:23 +0100

    fix(flask): remove debug=True and use FLASK_DEBUG env variable

--- ArithmeticApp/app.py
+++ ArithmeticApp/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -23,6 +25,6 @@ def calculate():
     return jsonify({'result': result})
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
+if __name__ == "__main__":
+    debug_mode = os.getenv("FLASK_DEBUG", "false").lower() == "true"
+    app.run(host="0.0.0.0", port=5000, debug=debug_mode) # nosec B104
